2d0e87315948cb38951bceda2ee86efaeb1744b8
Cari Licor <Carilicor@gmail.com>
2022-10-18 16:50:28 -0400

    updated

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,13 @@
 import functions from 'firebase-functions'
 import express from 'express'
+import { createUser, getAllUsers } from './src/users.js'
 
 
 const app = express()
+app.use(express.json())
+
+app.get('/users', getAllUsers)
+app.post('/users', createUser)
 
 app.get('/test', (req, res) => res.send('Our first cloud API works, MF!'))
 
